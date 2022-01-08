a750b43448b0a015cff8e8b4b9aa7e31d684866b
Next In <vedgupta@protonmail.com>
2022-01-08 13:40:19 +0000

    Updates

---
+++ precache-manifest.037a573a3a511d05715903dbc60555a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d6703cfb51c956035d2fe46ddbb7e9",
+    "url": "/PreNotebook/index.html"
+  },
+  {
+    "revision": "89032c694510903166ce",
+    "url": "/PreNotebook/static/css/main.8be0e3e8.chunk.css"
+  },
+  {
+    "revision": "4643dca3b306437438e4",
+    "url": "/PreNotebook/static/js/2.cbd1cf0c.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/PreNotebook/static/js/2.cbd1cf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adf1b88515899a96bf6",
+    "url": "/PreNotebook/static/js/3.864be9c7.chunk.js"
+  },
+  {
+    "revision": "89032c694510903166ce",
+    "url": "/PreNotebook/static/js/main.5de1bc25.chunk.js"
+  },
+  {
+    "revision": "8653a77c9a0156390537",
+    "url": "/PreNotebook/static/js/runtime-main.322a284c.js"
+  }
+]);
